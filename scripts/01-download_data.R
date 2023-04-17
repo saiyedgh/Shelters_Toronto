67618cd6fc4715b0c0de71a5b30f06bde7803db0
Syed Hassan <sai-yed@outlook.com>
2023-04-17 12:27:25 -0400

    with 2022 and 2021 data

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -23,16 +23,23 @@ resources <- list_package_resources("21c83b32-d5a8-4106-a54f-010dbe49f6f2")
 
 # identify datastore resources; by default, Toronto Open Data sets datastore resource format to CSV for non-geospatial and GeoJSON for geospatial resources
 datastore_resources <- filter(resources, tolower(format) %in% c('csv', 'geojson'))
+head(datastore_resources)
 
-# load the first datastore resource as a sample
-data <- filter(datastore_resources, row_number()==1) %>% get_resource()
-data
+# load the 2022 and 2021 datastore resources
+data_2022 <- filter(datastore_resources, row_number()==6) %>% get_resource()
+data_2022
+head(data_2022)
 
-head(data)
+data_2021 <- filter(datastore_resources, row_number()==4) %>% get_resource()
+data_2021
+
+head(data_2021)
+
 ###
 
 
 #### Save data ####
-write_csv(data, "inputs/data/raw_data.csv") 
+write_csv(data_2022, "inputs/data/raw_data_2022.csv")
+write_csv(data_2021, "inputs/data/raw_data_2021.csv")
 
          
